4c3a0d1d3a6c9ee1540a6626998a5c3285b41a0e
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 18:40:14 +0530

    Committing files

---
+++ CodeForces/Java 8/1047B/110771329.java
@@ -0,0 +1,105 @@
+//This code is written by प्रविण शंखपाळ 
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.PriorityQueue;
+import java.util.List;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.Vector;
+import javafx.util.Pair;
+
+public class Luna_Lovegood {
+
+	public static void main(String[] args) {
+
+		try {
+
+			FastReader fr = new FastReader();
+
+			int n = fr.nextInt();
+
+			long ans = 0;
+			long max = 0;
+
+			while (n > 0) {
+
+				long a = fr.nextLong();
+				long b = fr.nextLong();
+
+				ans = a + b;
+
+				max = Math.max(max, ans);
+
+				n--;
+			}
+
+			System.out.println(max);
+
+		} catch (
+
+		Exception e) {
+			return;
+		}
+
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
