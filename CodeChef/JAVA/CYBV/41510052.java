81e9bd0eb38377afb670446e6ac47093b07f050b
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 17:37:20 +0530

    Committing files

---
+++ CodeChef/JAVA/CYBV/41510052.java
@@ -0,0 +1,35 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	try {
+			Scanner sc = new Scanner(System.in);
+			int T = sc.nextInt();
+			while (T != 0) {
+				
+				long N = sc.nextLong();
+				long K =sc.nextLong();
+				long result = K/N;
+				System.out.println(result);
+				
+				
+				
+				
+				
+				
+				
+				T--;
+			}
+		} catch (Exception e) {
+			return;
+		}
+	}
+
+}
