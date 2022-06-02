5c8abbcd12ab71bbecd74e11c1dce1a399f28f9b
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 17:36:20 +0530

    Committing files

---
+++ CodeChef/JAVA/NEWBIE04/42628481.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Apple {
+
+	public static void main(String[] args) {
+
+		try {
+
+			Scanner sc = new Scanner(System.in);
+
+			String X = sc.next();
+
+			System.out.println("Hey" + " " + X + ", " + "Welcome to the world of CP");
+
+		} catch (Exception e) {
+
+			return;
+
+		}
+
+	}
+
+}
