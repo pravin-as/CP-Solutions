be6e58cf1ccac4c2cae38bcae6f10793619f1b09
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 17:33:23 +0530

    Committing files

---
+++ CodeChef/JAVA/CDHNT4/43080910.java
@@ -0,0 +1,79 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class FairchildTangerine {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+
+			FastReader s = new FastReader();
+			int T = s.nextInt();
+
+			while (T > 0) {
+
+				int N = s.nextInt();
+
+				long ans = 1;
+
+				for (int i = N; i > 0; i--) {
+					ans *= i;
+				}
+
+				System.out.println(ans);
+
+				T--;
+			}
+		} catch (
+
+		Exception e) {
+			return;
+
+		}
+	}
+}
