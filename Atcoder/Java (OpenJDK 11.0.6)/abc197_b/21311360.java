ff6992d3c1d9df07d9eaac495cf8ce40df2e57ba
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 20:06:25 +0530

    Committing files

---
+++ Atcoder/Java (OpenJDK 11.0.6)/abc197_b/21311360.java
@@ -0,0 +1,181 @@
+//This code is written by प्रविण शंखपाळ 
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.PriorityQueue;
+import java.util.List;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+ class Main {
+
+	public static void main(String[] args) {
+
+		try {
+
+			FastReader fr = new FastReader();
+
+			int h = fr.nextInt();
+			int w = fr.nextInt();
+			int x = fr.nextInt();
+			int y = fr.nextInt();
+
+			String arr[] = new String[h];
+
+			for (int i = 0; i < h; i++) {
+				arr[i] = fr.next();
+			}
+
+			int count = 0;
+
+			for (int i = y - 1; i >= 0; i--) {
+
+				if (arr[x - 1].charAt(i) == '.') {
+					count++;
+
+				} else {
+					break;
+				}
+
+			}
+
+			for (int i = y - 1; i < w; i++) {
+
+				if (arr[x - 1].charAt(i) == '.') {
+					count++;
+
+				} else {
+					break;
+				}
+
+			}
+			for (int i = x - 1; i >= 0; i--) {
+
+				if (arr[i].charAt(y - 1) == '.') {
+					count++;
+
+				} else {
+					break;
+				}
+
+			}
+
+			for (int i = x - 1; i < h; i++) {
+
+				if (arr[i].charAt(y - 1) == '.') {
+					count++;
+
+				} else {
+					break;
+				}
+
+			}
+
+			System.out.println(count - 3);
+
+		} catch (Exception e) {
+			return;
+		}
+	}
+
+	static class Pair implements Comparable<Pair> {
+		int a, b;
+
+		Pair(int a, int b) {
+			this.a = a;
+			this.b = b;
+		}
+
+		public int compareTo(Pair o) {
+			// TODO Auto-generated method stub
+			if (this.a != o.a)
+				return Integer.compare(this.a, o.a);
+			else
+				return Integer.compare(this.b, o.b);
+			// return 0;
+		}
+
+		public boolean equals(Object o) {
+			if (o instanceof Pair) {
+				Pair p = (Pair) o;
+				return p.a == a && p.b == b;
+			}
+			return false;
+		}
+
+	}
+
+	static int binarySearch(int arr[], int first, int last, int key) {
+		int mid = (first + last) / 2;
+		while (first <= last) {
+			if (arr[mid] < key) {
+				first = mid + 1;
+			} else if (arr[mid] == key) {
+				return mid;
+			} else {
+				last = mid - 1;
+			}
+			mid = (first + last) / 2;
+		}
+		return -1;
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
