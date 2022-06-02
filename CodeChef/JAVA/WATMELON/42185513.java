c6ac8863ce4039147d38df616be22fd50ebf1736
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 17:36:40 +0530

    Committing files

---
+++ CodeChef/JAVA/WATMELON/42185513.java
@@ -0,0 +1,40 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+try {
+			Scanner sc = new Scanner(System.in);
+			int T = sc.nextInt();
+			while (T > 0) {
+				int N = sc.nextInt();
+
+				long count = 0;
+				int A[] = new int[N];
+				for (int i = 0; i < N; i++) {
+					A[i] = sc.nextInt();
+				}
+				for (int i = 0; i < N; i++) {
+					count += A[i];
+				}
+				if (count >= 0) {
+					System.out.println("YES");
+				} else {
+					System.out.println("NO");
+				}
+
+				T--;
+			}
+
+		} catch (Exception e) {
+			return;
+		}
+
+	}
+}
