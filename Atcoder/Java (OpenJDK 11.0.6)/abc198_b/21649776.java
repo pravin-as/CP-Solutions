15f99e76e8bf2fe03dfbc6f4d2f356ce0b879e07
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 20:06:29 +0530

    Committing files

---
+++ Atcoder/Java (OpenJDK 11.0.6)/abc198_b/21649776.java
@@ -0,0 +1,217 @@
+//This code is written by प्रविण शंखपाळ 
+
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.PriorityQueue;
+import java.util.List;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+ class Main {
+
+	static boolean isPalindrome(String str) {
+
+		int i = 0, j = str.length() - 1;
+
+		while (i < j) {
+
+			if (str.charAt(i) != str.charAt(j))
+				return false;
+
+			i++;
+			j--;
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+
+		try {
+
+			FastReader fr = new FastReader();
+			BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+			String str = fr.next();
+			int n = str.length();
+			int count = 0;
+
+			for (int i = n - 1; i >= 0; i--) {
+
+				if (str.charAt(i) == '0') {
+
+					count++;
+
+				} else {
+
+					break;
+				}
+
+			}
+
+			String check = str.substring(0, n - count);
+
+			System.out.println(isPalindrome(check) ? "Yes" : "No");
+
+		} catch (
+
+		Exception e) {
+			return;
+		}
+	}
+
+	static void merge(int arr[], int l, int m, int r) {
+
+		int n1 = m - l + 1;
+		int n2 = r - m;
+
+		int L[] = new int[n1];
+		int R[] = new int[n2];
+
+		for (int i = 0; i < n1; ++i)
+			L[i] = arr[l + i];
+		for (int j = 0; j < n2; ++j)
+			R[j] = arr[m + 1 + j];
+
+		int i = 0, j = 0;
+
+		int k = l;
+		while (i < n1 && j < n2) {
+			if (L[i] <= R[j]) {
+				arr[k] = L[i];
+				i++;
+			} else {
+				arr[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+
+		while (i < n1) {
+			arr[k] = L[i];
+			i++;
+			k++;
+		}
+
+		while (j < n2) {
+			arr[k] = R[j];
+			j++;
+			k++;
+		}
+	}
+
+	static void sort(int arr[], int l, int r) {
+		if (l < r) {
+
+			int m = l + (r - l) / 2;
+
+			sort(arr, l, m);
+			sort(arr, m + 1, r);
+
+			merge(arr, l, m, r);
+		}
+	}
+
+	static class Pair implements Comparable<Pair> {
+		int a, b;
+
+		Pair(int a, int b) {
+			this.a = a;
+			this.b = b;
+		}
+
+		public int compareTo(Pair o) {
+			// TODO Auto-generated method stub
+			if (this.a != o.a)
+				return Integer.compare(this.a, o.a);
+			else
+				return Integer.compare(this.b, o.b);
+			// return 0;
+		}
+
+		public boolean equals(Object o) {
+			if (o instanceof Pair) {
+				Pair p = (Pair) o;
+				return p.a == a && p.b == b;
+			}
+			return false;
+		}
+	}
+
+	static int binarySearch(int arr[], int first, int last, int key) {
+		int mid = (first + last) / 2;
+		while (first <= last) {
+			if (arr[mid] < key) {
+				first = mid + 1;
+			} else if (arr[mid] == key) {
+				return mid;
+			} else {
+				last = mid - 1;
+			}
+			mid = (first + last) / 2;
+		}
+		return -1;
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
