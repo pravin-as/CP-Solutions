5f778c67411352bfcec525be8ddbf17ffc290e82
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 20:09:12 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc244_d/30392227.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool sign(string S){
+    return S == "R G B" || S == "G B R" || S == "B R G";
+}
+int main(){
+    string S, T;
+    getline(cin, S);
+    getline(cin, T);
+    if(sign(S) == sign(T)) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
