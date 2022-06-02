a9e9120b0381286522cba293ad0317e4f9fbd962
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 20:07:32 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc222_c/26460411.cpp
@@ -0,0 +1,179 @@
+//This code is written by प्रविण शंखपाळ 
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define flash ios_base::sync_with_stdio(0);cin.tie(0);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((ll)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define srt(x) sort(x.begin(),x.end())
+
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+typedef tree<pair<ll, ll>, null_type, less<pair<ll, ll>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+ 
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+ll power(ll a, ll b){ll ans=1; while(b){if(b&1)ans = (ans*a)%MOD;a = (a*a)%MOD;b>>=1;} return ans;}
+ll power1(ll a, ll b){ll ans=1; while(b){if(b&1)ans = (ans*a);a = (a*a);b>>=1;} return ans;}
+
+
+void solve(){
+
+    // use brackets for binary operations.
+
+    ll t=1;
+   // cin>>t;
+
+    while(t--){ 
+
+        ll n, m;
+        cin >> n >> m;
+
+        vector<vector<char>> A(2*n,vector<char>(m));
+
+        for(ll i = 0 ; i < 2*n ; i++){
+            for(ll j = 0 ; j < m ; j++){
+                cin >> A[i][j];
+            }
+        }
+        debug(A)
+
+        vector<tuple<ll,ll,char>> temp(2*n);
+
+        for(ll i = 0 ; i < 2*n; i++){
+            temp[i] = {0LL,i,A[i][0]};
+        }
+
+     /*   for(ll i = 0 ; i< 2*n;i++){
+            cout << get<0>(temp[i]) << get<1>(temp[i])+1 << get<2>(temp[i]) << "\n";
+        }
+        cout << "\n";*/
+     
+
+
+    for(ll j = 1 ; j <= m ; j++){
+
+        for(ll i = 0; i < 2*n ; i+=2){
+
+            if(get<2>(temp[i]) == 'G'){
+                if(get<2>(temp[i+1]) == 'C'){
+                    get<0>(temp[i])++;
+                }else if(get<2>(temp[i+1]) == 'P'){
+                    get<0>(temp[i+1])++;
+                }
+                if(j!=m){
+                get<2>(temp[i]) = A[get<1>(temp[i])][j];
+                get<2>(temp[i+1]) = A[get<1>(temp[i+1])][j];}
+            }
+            else if(get<2>(temp[i]) == 'C'){
+                if(get<2>(temp[i+1]) == 'P'){
+                    get<0>(temp[i])++;
+                }else if(get<2>(temp[i+1]) == 'G'){
+                    get<0>(temp[i+1])++;
+                }
+                if(j!=m){
+                get<2>(temp[i]) = A[get<1>(temp[i])][j];
+                get<2>(temp[i+1]) = A[get<1>(temp[i+1])][j];}
+            }
+            else{
+                if(get<2>(temp[i+1]) == 'G'){
+                    get<0>(temp[i])++;
+                }else if(get<2>(temp[i+1]) == 'C'){
+                    get<0>(temp[i+1])++;
+                }
+                if(j!=m){
+                get<2>(temp[i]) = A[get<1>(temp[i])][j];
+                get<2>(temp[i+1]) = A[get<1>(temp[i+1])][j];}
+            }
+
+            
+
+        }
+
+        std::sort(begin(temp), end(temp), 
+    [](tuple<ll,ll,char> const &t1, tuple<ll,ll,char> const &t2) {
+            if(get<0>(t1) == get<0>(t2)){
+                return get<1>(t1) < get<1>(t2);
+            }
+         return get<0>(t1) > get<0>(t2); 
+    }
+      );
+
+      /*  for(ll i = 0 ; i< 2*n;i++){
+            cout << get<0>(temp[i]) << get<1>(temp[i])+1 << get<2>(temp[i]) << "\n";
+        }
+        cout << "\n";*/
+
+        
+
+    }
+
+        for(ll i = 0 ; i< 2*n;i++){
+            cout <<  get<1>(temp[i]) + 1 << "\n";
+        }
+
+  
+     
+    }
+        }
+    
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    flash
+    solve();
+  //  cout<<fixed<<setprecision(10)<<mx<<"\n";
+
+}
+
+
+
