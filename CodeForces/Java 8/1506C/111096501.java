827a8a0fdcba107dcba98fd064d18a8efbc1e21b
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 18:40:01 +0530

    Committing files

---
+++ CodeForces/Java 8/1506C/111096501.java
@@ -0,0 +1,158 @@
+//This code is written by प्रविण शंखपाळ 
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.PriorityQueue;
+import java.util.List;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+public class Ginny_Weasley {
+
+	static int LCSubStr(char X[], char Y[], int m, int n) {
+
+		int LCStuff[][] = new int[m + 1][n + 1];
+
+		int result = 0;
+
+		for (int i = 0; i <= m; i++) {
+			for (int j = 0; j <= n; j++) {
+				if (i == 0 || j == 0)
+					LCStuff[i][j] = 0;
+				else if (X[i - 1] == Y[j - 1]) {
+					LCStuff[i][j] = LCStuff[i - 1][j - 1] + 1;
+					result = Integer.max(result, LCStuff[i][j]);
+				} else
+					LCStuff[i][j] = 0;
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+
+		try {
+
+			FastReader fr = new FastReader();
+
+			int T = fr.nextInt();
+
+			while (T > 0) {
+
+				String X = fr.next();
+				String Y = fr.next();
+
+				int m = X.length();
+				int n = Y.length();
+
+				System.out.println(m + n - 2 * LCSubStr(X.toCharArray(), Y.toCharArray(), m, n));
+
+				T--;
+			}
+
+		} catch (Exception e) {
+			return;
+		}
+	}
+
+	static class Pair implements Comparable<Pair> {
+		int a, b;
+
+		Pair(int a, int b) {
+			this.a = a;
+			this.b = b;
+		}
+
+		public int compareTo(Pair o) {
+			// TODO Auto-generated method stub
+			if (this.a != o.a)
+				return Integer.compare(this.a, o.a);
+			else
+				return Integer.compare(this.b, o.b);
+			// return 0;
+		}
+
+		public boolean equals(Object o) {
+			if (o instanceof Pair) {
+				Pair p = (Pair) o;
+				return p.a == a && p.b == b;
+			}
+			return false;
+		}
+
+	}
+
+	static int binarySearch(int arr[], int first, int last, int key) {
+		int mid = (first + last) / 2;
+		while (first <= last) {
+			if (arr[mid] < key) {
+				first = mid + 1;
+			} else if (arr[mid] == key) {
+				return mid;
+			} else {
+				last = mid - 1;
+			}
+			mid = (first + last) / 2;
+		}
+		return -1;
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
