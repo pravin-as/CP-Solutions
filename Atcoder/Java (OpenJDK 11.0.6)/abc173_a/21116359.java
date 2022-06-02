3b1b49cea3f8825a6c51b44afa8eebce93b14714
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 20:06:22 +0530

    Committing files

---
+++ Atcoder/Java (OpenJDK 11.0.6)/abc173_a/21116359.java
@@ -0,0 +1,98 @@
+//This code is written by प्रविण शंखपाळ 
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.PriorityQueue;
+import java.util.List;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+ class Main {
+
+	public static void main(String[] args) {
+
+		try {
+			FastReader fr = new FastReader();
+
+			long n = fr.nextLong();
+
+			long count = 0;
+
+			for (long i = 1;; i++) {
+
+				if (Long.parseLong(String.valueOf(i) + String.valueOf(i)) > n) {
+
+					count = i - 1;
+					break;
+
+				}
+
+			}
+
+			System.out.println(count);
+
+		} catch (Exception e) {
+			return;
+		}
+
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
