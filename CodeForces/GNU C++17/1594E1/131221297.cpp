604ae6ab65a5d077fc5646cf91f620f17cced5c4
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 19:05:53 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1594E1/131221297.cpp
@@ -0,0 +1,132 @@
+//This code is written by प्रविण शंखपाळ 
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define flash ios_base::sync_with_stdio(0);cin.tie(0);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((ll)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define srt(x) sort(x.begin(),x.end())
+
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+typedef tree<pair<ll, ll>, null_type, less<pair<ll, ll>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+ 
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+ll power(ll x, ll y,ll p)
+{
+    ll res = 1;     
+ 
+    x = x % p; 
+  
+    if (x == 0) return 0; 
+ 
+    while (y > 0)
+    {
+         
+        if (y & 1)
+            res = (res*x) % p;
+ 
+         
+        y = y>>1;  
+        x = (x*x) % p;
+    }
+    return res;
+}
+
+void solve(){
+
+    // use brackets for binary operations.
+
+    ll t=1;
+ //   cin>>t;
+
+    while(t--){ 
+
+        
+
+        ll n;
+        cin >> n;
+
+        ll ans = 6LL, po = 1;
+
+        for(ll i = 0 ; i < n ; i++){
+            po *= 2LL;
+        }
+
+       
+
+
+
+        ans *= power(4, po - 2, MOD);
+        ans %= MOD;
+
+        cout << ans << "\n";
+
+  
+     
+    }
+        }
+    
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    flash
+    solve();
+  //  cout<<fixed<<setprecision(10)<<mx<<"\n";
+
+}
+
+
+
+
