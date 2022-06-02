54159af41cc9d111b72aeb2cbebf0e453f1ad9b1
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 18:56:39 +0530

    Committing files

---
+++ CodeForces/Java 11/1305A/116721418.java
@@ -0,0 +1,211 @@
+//This code is written by प्रविण शंखपाळ 
+
+//package wizard;
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Ginny_Weasley {
+
+	public static void main(String[] args) {
+
+		try {
+
+			FastReader fr = new FastReader();
+			PrintWriter pt = new PrintWriter(System.out);
+
+			int t = fr.nextInt();
+			ArrayList<Tuple> x = new ArrayList<>();
+
+			while (t > 0) {
+
+				int n = fr.nextInt();
+				int A[] = new int[n];
+				int B[] = new int[n];
+
+				for (int i = 0; i < n; i++) {
+					A[i] = fr.nextInt();
+				}
+				for (int i = 0; i < n; i++) {
+					B[i] = fr.nextInt();
+				}
+
+				sort(A, 0, n - 1);
+				sort(B, 0, n - 1);
+
+				for (int i = 0; i < n; i++) {
+					pt.print(A[i] + " ");
+				}
+				pt.println();
+				for (int i = 0; i < n; i++) {
+					pt.print(B[i] + " ");
+				}
+
+				pt.println();
+
+				t--;
+
+			}
+
+			pt.close();
+
+		} catch (
+
+		Exception e) {
+			return;
+		}
+	}
+
+	static class Tuple<Tuple> {
+
+		int a;
+		int b;
+		int c;
+
+		Tuple(int a, int b, int c) {
+			this.a = a;
+			this.b = b;
+			this.c = c;
+		}
+
+	}
+
+	static void merge(int arr[], int l, int m, int r) {
+
+		int n1 = m - l + 1;
+		int n2 = r - m;
+
+		int L[] = new int[n1];
+		int R[] = new int[n2];
+
+		for (int i = 0; i < n1; ++i)
+			L[i] = arr[l + i];
+		for (int j = 0; j < n2; ++j)
+			R[j] = arr[m + 1 + j];
+
+		int i = 0, j = 0;
+
+		int k = l;
+		while (i < n1 && j < n2) {
+			if (L[i] <= R[j]) {
+				arr[k] = L[i];
+				i++;
+			} else {
+				arr[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+
+		while (i < n1) {
+			arr[k] = L[i];
+			i++;
+			k++;
+		}
+
+		while (j < n2) {
+			arr[k] = R[j];
+			j++;
+			k++;
+		}
+	}
+
+	static void sort(int arr[], int l, int r) {
+		if (l < r) {
+
+			int m = l + (r - l) / 2;
+
+			sort(arr, l, m);
+			sort(arr, m + 1, r);
+
+			merge(arr, l, m, r);
+		}
+	}
+
+	static class Pair implements Comparable<Pair> {
+		int a, b;
+
+		Pair(int a, int b) {
+			this.a = a;
+			this.b = b;
+		}
+
+		public int compareTo(Pair o) {
+			// TODO Auto-generated method stub
+			if (this.a != o.a)
+				return Long.compare(this.a, o.a);
+			else
+				return Long.compare(this.b, o.b);
+			// return 0;
+		}
+
+		public boolean equals(Object o) {
+			if (o instanceof Pair) {
+				Pair p = (Pair) o;
+				return p.a == a && p.b == b;
+			}
+			return false;
+		}
+
+	}
+
+	static int binarySearch(int arr[], int first, int last, long key) {
+		int mid = (first + last) / 2;
+		while (first <= last) {
+			if (arr[mid] < key) {
+				first = mid + 1;
+			} else if (arr[mid] == key) {
+				return mid;
+			} else {
+				last = mid - 1;
+			}
+			mid = (first + last) / 2;
+		}
+//		return -1;
+		return mid;
+//		return mid if want to find key greater than or less than arr[mid] if exact value is not given
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
