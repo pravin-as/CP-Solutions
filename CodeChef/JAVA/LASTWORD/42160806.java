ab7763602a291dcf53ed699c2779c55c7083054b
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 17:33:54 +0530

    Committing files

---
+++ CodeChef/JAVA/LASTWORD/42160806.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		try {
+			Scanner sc = new Scanner(System.in);
+			String str = sc.nextLine();
+			String surname = str.substring(str.lastIndexOf(" ") + 1);
+
+			System.out.println(surname.length());
+
+		} catch (Exception e) {
+			return;
+		}
+
+	}
+}
+
