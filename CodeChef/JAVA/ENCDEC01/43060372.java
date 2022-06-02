7677d35c5197b1aaa0dd7814f69c703f2a2d3b67
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 17:36:08 +0530

    Committing files

---
+++ CodeChef/JAVA/ENCDEC01/43060372.java
@@ -0,0 +1,86 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class FairchildTangerine {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+
+			FastReader s = new FastReader();
+			int T = s.nextInt();
+
+			while (T > 0) {
+
+				int N = s.nextInt();
+				int X = s.nextInt();
+
+				int A[] = new int[N];
+
+				for (int i = 0; i < N; i++) {
+					A[i] = s.nextInt();
+				}
+
+				Arrays.sort(A);
+
+				if (A[N - 1] - A[0] < X) {
+					System.out.println("YES");
+				} else {
+					System.out.println("NO");
+				}
+
+				T--;
+			}
+		} catch (
+
+		Exception e) {
+			return;
+
+		}
+	}
+}
