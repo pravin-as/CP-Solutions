4249ab17c85beee26c70dabf80ec4d1c01269f35
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 19:04:35 +0530

    Committing files

---
+++ CodeForces/GNU C++17/16C/142635300.cpp
@@ -0,0 +1,159 @@
+//This code is written by प्रविण शंखपाळ
+ 
+ 
+ 
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define flash ios_base::sync_with_stdio(0);cin.tie(0);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF (ll)2e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((ll)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define srt(x) sort(x.begin(),x.end())
+ 
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+typedef tree<pair<ll, ll>, null_type, less<pair<ll, ll>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+ 
+ 
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+// while(l <= r){ll mid = l + (r - l) / 2;if(check(mid)){ans = mid;l = mid + 1;}else{r = mid - 1;}}    // TTTTFFFF....
+// while(r - l > (double)1e-9){double mid = l + (r - l) / 2.0;if(cond(mid)){ans = mid;l = mid;}else{r = mid;}}  // TTTTFFFF....
+// generally check function is O(n) in most problems and mid is the value we are checking in binary search
+// a|b = a⊕b + a&b      a⊕(a&b) = (a|b)⊕b       b⊕(a&b) = (a|b)⊕a       (a&b)⊕(a|b) = a⊕b
+// a+b = a|b + a&b      a+b = a⊕b + 2(a&b)
+// a-b = (a⊕(a&b))-((a|b)⊕a)    a-b = ((a|b)⊕b)-((a|b)⊕a)   a-b = (a⊕(a&b))-(b⊕(a&b))   a-b = ((a|b)⊕b)-(b⊕(a&b))
+ 
+ll power(ll a, ll b){ll ans=1; while(b){if(b&1)ans = (ans*a)%MOD;a = (a*a)%MOD;b>>=1;} return ans;}
+ll power1(ll a, ll b){ll ans=1; while(b){if(b&1)ans = (ans*a);a = (a*a);b>>=1;} return ans;}
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0)n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll dx[] = {1,0,-1,0,1,-1,1,-1};
+ll dy[] = {0,1,0,-1,1,1,-1,-1};
+// bool isvalid(ll x, ll y){if(x>=1 && x <= n && y >= 1 && y <= m && mat[x][y] != '#' && vis[x][y] == 0){return true;}return false;}
+// string ds = "RLDU";
+// vector<vector<ll>> adj, adj2;
+// vector<ll>  dp;
+// vector<ll> vis;
+// vector<vector<char>> mat;
+
+
+ll n, m; 
+vector<ll> A;
+
+
+
+
+
+ 
+void solve(){
+ 
+    ll TC = 1;
+    // cin >> TC;
+ 
+ 
+ 
+    while(TC--){
+ 
+    
+
+           ll a, b, x, y, ans1, ans2; cin >> a >> b >> x >> y;
+           ll gc = __gcd(x,y); x /= gc; y /= gc; ans2 = min(a/x,b/y);
+            
+           cout << (ans2*x) << " " << (ans2*y) << "\n"; 
+         
+ 
+ 
+ 
+        // when order does not matter always sort the vector.
+        // States in dp may or may not contain index. So first think about whether order is important or just number of objects is important.Also state transition can be like i -> x and x + 1 -> j.
+        // min-max means binary search. Sometimes binary search is not direct and it's better to check on which variable binary search should be applied so that checker function is easy to made. when you can't think about anything else think about binary search on answer.
+        // In for-loop you can use two variables together in single loop. Also sometimes loop over size of window rather than two varibles (l and r).
+        // if the value of some variable is very less like < 20 always think of dp with bitmasking.
+        // you can apply n loops using recursion. Steps are calling the function then putting a loop in that function and calling the function again in that loop.
+        // Check any condition in recursion when that step actually comes. Don't unnecesary check initially only.
+        // sometimes its better to think from back. Also mapping every value from 1 to 10^5 is very helpful.
+        // use difference arrays. Expected value and dearrangemts are always dp.
+        // check if it is a graph question or a tree question.
+        // count the number of subsequences is mostly dp. Always make dp of n+1.
+        // use brackets for binary operations.
+        // use .lower_bound()
+        // __lg for log
+        // Think about all data structures first and which will be easy
+        // to implement. vector, set, multiset, unordered_set, ordered_set(pbds),
+        // map, multimap, unordered_map(custom hashing), Queue, priority_queue, stack.
+        // Always define vector in global if passing in case of bfs, dfs specifically.
+        
+        
+            
+            }
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+ 
+     flash
+    solve();
+  //  cout<<fixed<<setprecision(10)<<mx<<"\n";
+  //  divide by 2.0 to prevent precision errors
+  
+}
