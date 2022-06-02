fd386bb7f8602fee2f0c7f61f4e7e92530989c1d
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 19:05:39 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1598D/131615251.cpp
@@ -0,0 +1,165 @@
+//This code is written by प्रविण शंखपाळ 
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define flash ios_base::sync_with_stdio(0);cin.tie(0);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((ll)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define srt(x) sort(x.begin(),x.end())
+
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+typedef tree<pair<ll, ll>, null_type, less<pair<ll, ll>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+ 
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+ll power(ll a, ll b){ll ans=1; while(b){if(b&1)ans = (ans*a)%MOD;a = (a*a)%MOD;b>>=1;} return ans;}
+ll power1(ll a, ll b){ll ans=1; while(b){if(b&1)ans = (ans*a);a = (a*a);b>>=1;} return ans;}
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+
+ll RightAngled(vector<vector<ll>>& a, ll n)
+{
+ 
+    
+    unordered_map<ll, ll, custom_hash> xpoints;
+    unordered_map<ll, ll, custom_hash> ypoints;
+ 
+    for (int i = 0; i < n; i++) {
+        xpoints[a[i][0]]++;
+        ypoints[a[i][1]]++;
+    }
+ 
+    
+   ll count = 0;
+ 
+    
+    for (int i = 0; i < n; i++) {
+ 
+        if (xpoints[a[i][0]] >= 1
+            && ypoints[a[i][1]] >= 1) {
+ 
+          
+            count += (xpoints[a[i][0]] - 1)
+                     * (ypoints[a[i][1]] - 1);
+        }
+    }
+ 
+    
+    return count;
+}
+
+
+
+void solve(){
+
+    // use brackets for binary operations.
+
+    ll t=1;
+    cin>>t;
+
+    while(t--){ 
+
+       
+        ll n;
+        cin >> n;
+
+        vector<vector<ll>> A;
+
+        for(ll i = 0 ; i < n ;  i++){
+            ll temp1, temp2;
+            cin >> temp1 >> temp2;
+            vector<ll> p;
+            p.push_back(temp1);
+            p.push_back(temp2);
+            A.push_back(p);
+        }
+
+
+        cout <<( ((n * (n - 1LL) * (n - 2LL)) / 6LL) - RightAngled(A, n) )<< "\n";
+
+
+
+
+        
+
+
+
+        
+
+
+
+    }
+        }
+    
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    flash
+    solve();
+  //  cout<<fixed<<setprecision(10)<<mx<<"\n";
+
+}
