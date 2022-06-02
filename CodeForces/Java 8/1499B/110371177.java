618dfff3a448550b02e3ac8d00cd9903fb1d942f
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 18:40:38 +0530

    Committing files

---
+++ CodeForces/Java 8/1499B/110371177.java
@@ -0,0 +1,108 @@
+//This code is written by प्रविण शंखपाळ 
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.PriorityQueue;
+import java.util.List;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.Vector;
+import javafx.util.Pair;
+
+public class Ginny_Weasley {
+
+	public static void main(String[] args) {
+		try {
+
+			FastReader fr = new FastReader();
+
+			int T = fr.nextInt();
+
+			while (T > 0) {
+
+				String str = fr.next();
+
+				int x = -1;
+
+				for (int i = 0; i < str.length() - 1; i++) {
+
+					if (str.charAt(i) == '1') {
+
+						if (str.charAt(i + 1) == '1') {
+
+							x = i + 1;
+							break;
+						}
+					}
+
+				}
+
+				System.out.println((x == -1) ? "YES" : (str.substring(x).contains("00") ? "NO" : "YES"));
+
+				T--;
+			}
+
+		} catch (Exception e) {
+			return;
+		}
+
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
