130ac480e9d5cc9c36500cdebceadbff78094f6c
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 17:36:24 +0530

    Committing files

---
+++ CodeChef/JAVA/ITGUY84/42219856.java
@@ -0,0 +1,44 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	try {
+			Scanner sc = new Scanner(System.in);
+
+			int T = sc.nextInt();
+			while (T > 0) {
+				int N = sc.nextInt();
+				int l = N;
+				char alphabet = 'A';
+				for (int i = 1; i <= N; i++) {
+
+					for (int j = 1; j <= N; j++) {
+						if (j <= i) {
+							System.out.print(l);
+
+						} else {
+							System.out.print(alphabet);
+
+						}
+					}
+					l--;
+					alphabet++;
+					System.out.println();
+				}
+
+				T--;
+			}
+
+		} catch (Exception e) {
+			return;
+		}
+
+	}
+}
