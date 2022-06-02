9bf3406fd5cefa4c917e5656f2f7c9ee9d746c45
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 20:06:12 +0530

    Committing files

---
+++ Atcoder/Java (OpenJDK 11.0.6)/abc185_a/20597657.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			FastReader s = new FastReader();
+
+			int a = s.nextInt();
+			int b = s.nextInt();
+			int c = s.nextInt();
+			int d = s.nextInt();
+
+			System.out.println(Math.min(a, Math.min(b, Math.min(c, d))));
+
+		} catch (Exception e) {
+			return;
+		}
+
+	}
+}
