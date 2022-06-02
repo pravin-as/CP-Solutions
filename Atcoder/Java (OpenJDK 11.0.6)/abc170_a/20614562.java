574a3167659e27efb92a5153c199e18b312f3fdb
Pravin Shankhapal <76400994+pravin-as@users.noreply.github.com>
2022-06-02 20:06:12 +0530

    Committing files

---
+++ Atcoder/Java (OpenJDK 11.0.6)/abc170_a/20614562.java
@@ -0,0 +1,86 @@
+//This code is written by प्रविण शंखपाळ 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+class Main {
+
+	public static void main(String[] args) {
+		try {
+			FastReader fr = new FastReader();
+
+			Vector<Integer> vec = new Vector<Integer>();
+
+			for (int i = 0; i < 5; i++) {
+
+				vec.add(fr.nextInt());
+
+			}
+
+			for (int i = 0; i < 5; i++) {
+
+				if (vec.get(i) == 0) {
+					System.out.println(i+1);
+					break;
+				}
+
+			}
+
+		} catch (Exception e) {
+			return;
+		}
+
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
